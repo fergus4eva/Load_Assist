895b6377ff33b1267cbf73c6302559ac85ae2e34
fergus4eva <158233973+fergus4eva@users.noreply.github.com>
2026-03-04 14:01:43 -0500

    Added Logo, login and password both work as separate var text and text2.  Played around with modifiers, contentScale, working with images.

--- app/src/main/java/com/example/loadassist/MainActivity.kt
+++ app/src/main/java/com/example/loadassist/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Arrangement.Center
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -19,7 +21,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.loadassist.ui.theme.LoadAssistTheme
 
 
@@ -42,11 +47,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Login(name: String, modifier: Modifier = Modifier) {
-    var text by remember { mutableStateOf("") }
+    var text by remember { mutableStateOf("")}
+    var text2 by remember { mutableStateOf("")}
+    Column(modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo",
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier.size(500.dp)
+        )
+
+    }
+
+
 
     Column(modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,horizontalAlignment = Alignment.CenterHorizontally) {
+
         Text(
-            text = "Login: !"
+            text = "Login: "
         )
         TextField(
             value = text,
@@ -57,8 +75,8 @@ fun Login(name: String, modifier: Modifier = Modifier) {
             text = "Password: "
         )
         TextField(
-            value = text,
-            onValueChange = { text = it },
+            value = text2,
+            onValueChange = { text2 = it },
             label = { Text("") }
         )
     }
