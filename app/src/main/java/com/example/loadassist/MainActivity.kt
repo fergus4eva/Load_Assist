a1d99bcc5ec51ef299dbe4254fb54a446d6e2148
fergus4eva <158233973+fergus4eva@users.noreply.github.com>
2026-03-03 22:06:03 -0500

    Initial Commit - learning how to structure Android Code using Developer Documentation - a basic project

---
+++ app/src/main/java/com/example/loadassist/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.loadassist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.loadassist.ui.theme.LoadAssistTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LoadAssistTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var text by remember { mutableStateOf("") }
+
+    Column(modifier = modifier) {
+        Text(
+            text = "Hello $name!"
+        )
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("") }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LoadAssistTheme {
+        Greeting("Android")
+    }
+}
